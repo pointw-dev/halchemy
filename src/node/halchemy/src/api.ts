7cabc6b42d6cd7f7d547b1dbb617ddc723d93bbc
Michael Ottoson <michael@pointw.com>
2023-07-04 22:20:05 -0400

    finalized packaging stuff

--- src/node/halchemy/src/api.ts
+++ src/node/halchemy/src/api.ts
@@ -1,5 +1,34 @@
+import axios from "axios"
+
 export class Api {
-    public get() : string {
-        return 'hello, world'
+
+    private readonly baseApiUrl: string = 'http://localhost:2112'
+    private headers: object = {}
+
+    constructor(baseApiUrl: string, auth: string='Basic cm9vdDpwYXNzd29yZA==') {
+        this.headers = {
+            Accept: 'application/json',
+            'Content-type': 'application/json',
+            Authorization: auth
+        }
+
+        this.baseApiUrl = baseApiUrl  // TODO: do I need to hold onto this?
+        axios.defaults.baseURL = baseApiUrl
+    }
+
+    urlFromRel({resource, rel, parameters = {}, template = {}}: {
+        resource: object,
+        rel: string,
+        parameters?: object,
+        template?: object
+    }) {
+        let url = resource._links[rel].href
+
+    }
+
+    async get(url:string = '/'): Promise<{}> {
+        const response = await axios.get(url, {headers: this.headers})
+        // TODO: handle issues
+        return response
     }
 }
