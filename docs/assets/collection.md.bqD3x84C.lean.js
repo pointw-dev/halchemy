f4bea36aeeca921cbe139f1cfa95cfbe50761e75
Michael Ottoson <michael@pointw.com>
2024-04-30 23:02:04 -0400

    First vitepress docs

---
+++ docs/assets/collection.md.bqD3x84C.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,a as i,o as a,aa as n}from"./chunks/framework.o1OYiBFT.js";const g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"collection.md","filePath":"collection.md"}'),t={name:"collection.md"},h=n("",1),l=[h];function k(p,e,r,E,d,F){return a(),i("div",null,l)}const y=s(t,[["render",k]]);export{g as __pageData,y as default};
